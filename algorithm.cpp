a7748dc205b2abd3dcc4f40a1cf4233f66c385ca
kimminsu38oo <kimminsu38oo@gmail.com>
2024-03-17 10:10:41 +0900

    4375

---
+++ algorithm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int n;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	while (cin >> n) {
+		int cnt = 1, t = 1;
+		while (t % n != 0) {
+			cnt++;
+			t = t * 10 + 1;
+			t %= n;
+		}
+		cout << cnt << "\n";
+	}
+}
